a815a4ccca65d4ac1056b6d35a1519a0802bca9a
Danil <danil.fertov.2024@mail.ru>
2023-04-21 13:22:38 +0700

    add DeleteItem

---
+++ museum/DeleteItem.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace museum
+{
+    /// <summary>
+    /// Логика взаимодействия для DeleteItem.xaml
+    /// </summary>
+    public partial class DeleteItem : Window
+    {
+        public DeleteItem()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+        private void LoadData()
+        {
+            ItemGrid.ItemsSource = Helper.db.Items.Where(t => t.IdItem >= 1).Include(u => u.Museum).ToList();
+        }
+
+        private void DeleteItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Вы действительно хотите удалиь запись об экспонате?", "Уведомление", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    ;
+                    var Item = ItemGrid.SelectedItem as Item;
+                    Helper.db.Items.Remove(Item); Helper.db.SaveChanges();
+                    ItemGrid.ItemsSource = Helper.db.Items.ToList();
+                    MessageBox.Show("Запись успешно удалена");
+                }
+                catch
+                {
+                    MessageBox.Show("Не удалось удалить запись!", "Ошибка удаления записи!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+        }
+    }
+}
